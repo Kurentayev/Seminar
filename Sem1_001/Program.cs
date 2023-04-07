d6d67abb0d74e31d1a3e21d89906ceef12829c2f
Kurentayev <280200@list.ru>
2023-04-07 23:57:54 +0600

    Sem1 001

---
+++ Sem1_001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput); // метод Parse что делает? преобразует строку в число
+int result = userNumber * userNumber;
+Console.WriteLine($"{userNumber} -> {result}"); // интерполяция строк выводит переменые в числа ($)
+
+
